aac36f39b6fed75b7c728c3b9f2b666e3d68cdc6
Ma1czi <04miki@wp.pl>
2024-11-10 15:50:15 +0100

    add new function V in time and S in time

--- skrypt/skrypt.js
+++ skrypt/skrypt.js
@@ -13,41 +13,106 @@ function oblicz(){
     let h = [];
     let vs = [];
     let x = [];
+    let t = [];
+    let lastt=0;
+    let vt = [];
+    var sinpcos = 9.8* (Math.sin(a)+f*Math.cos(a));
+    var sinmcos = 9.8* (Math.sin(a)-f*Math.cos(a));
+    var sina = Math.sin(a);
+
+    vt.push(v);
+    t.push(0);
+    vt.push(0);
+    t.push((v)/(sinpcos));
+    lastt = (v)/(sinpcos);
+    x.push(0);
+    vs.push(v);
+    h.push(sina*s)
+
     while(s>eps){
-        v = Math.sqrt(19.6*s*(Math.sin(a)-f*Math.cos(a)));
+        v = Math.sqrt(2*s*(sinmcos));
         vs.push(v);
-        s = v*v/(19.6*(Math.sin(a)+f*Math.cos(a)));
-        console.log(s);
-        h.push(s);
+        vt.push(v);
+        t.push(lastt + (v)/(sinmcos));
+        lastt += (v)/(sinmcos);
+
+        s = v*v/(2*(sinpcos));
+        vt.push(0);
+        t.push(lastt + (v)/(sinpcos));
+        lastt += (v)/(sinpcos);
+        h.push(sina*s);
         suma++;
         x.push(suma);
     }
     
     ile.innerHTML = suma;
-    console.log(vs);
-    console.log(h);
-    console.log(suma);
+    // console.log(vs);
+    // console.log(h);
+    // console.log(suma);
+    // console.log(t);
+    // console.log(vt);
+    
+    // Define Data
+    const data = [
+      {x: x, y: h, mode:"lines", name:"Wykres wysokości"},
+      {x: x, y: vs, mode:"lines", name:"Wykres prędkości"}
+    ];
+    
+    //Define Layout
+    const layout = {
+        xaxis: {title: "Powtórzenia"},
+        yaxis: {title: "Wysokość[m] lub prędkość[m/s]"},
+        title: "Wykresy prędkości początkowej i maxymalnej wysokości od powtórzeń"
+    };
+    
+    // Display using Plotly
+    Plotly.newPlot("myPlot", data, layout);
 
-    const xValues = x;
-    const yValues = h;
+    
+    let y = [];
+    let xvt = [];
+    let xst = [];
+    let change = 0;
+    let j = 0;
+    let smax =0;
+    console.log(lastt);
+    console.log(t);
 
-    new Chart("myChart", {
-    type: "line",
-    data: {
-        labels: xValues,
-        datasets: [{
-        fill: false,
-        lineTension: 0,
-        backgroundColor: "rgba(0,0,255,1.0)",
-        borderColor: "rgba(0,0,255,0.1)",
-        data: yValues
-        }]
-    },
-    options: {
-        legend: {display: false},
-        scales: {
-        yAxes: [{ticks: {min: 0, max:h[0]}}],
+    for(let i = 0; i<=lastt; i+=0.1){
+        let z = i - t[j];
+        if(change == 0){
+            xvt.push(vt[j] - sinpcos*z);
+            xst.push(vt[j]*z - (sinpcos*z*z)/2);
+            if(vt[j] - sinpcos*z <=0){
+                change = 1;
+                smax = vt[j]*z - (sinpcos*z*z)/2;
+                j++;
+            }
+        }else{
+            xvt.push(sinmcos*z);
+            xst.push(smax - (sinmcos*z*z)/2);
+            if(0 + sinmcos*z >=vt[j+1]){
+                change = 0;
+                j++;
+            }
         }
+        y.push(i); 
     }
-    });
+    console.log(xvt)
+    console.log(y);
+    console.log(sinpcos);
+    console.log(sinmcos);
+    console.log(vt);
+    console.log(xst);
+    const datav = [
+        {x:y, y:xvt, mode:"lines", name: "Wykres prędkości od czasu"},
+        {x:y, y:xst, mode:"lines", name: "Wykres drogi od czasu"}
+    ];
+    const layoutv = {
+        xaxis: {title: "Czas [s]"},
+        yaxis: {title: "Prędkość [m/s] / Droga [m]"},
+        title: "Wykres Prędkości od czasu i Drogi od czasu"
+    };
+
+    Plotly.newPlot("vint", datav, layoutv);
 }
